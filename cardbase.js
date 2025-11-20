6ef416e857ac770f9616d99512123c4d9762c262
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-11-20 10:52:56 +0300

    deck

---
+++ cardbase.js
@@ -0,0 +1,90 @@
+const deck = [
+  {
+    file: 'maj00.jpg',
+    name: 'The Fool',
+  },
+  {
+    file: 'maj01.jpg',
+    name: 'The Magician',
+  },
+  {
+    file: 'maj02.jpg',
+    name: 'The High Priestess',
+  },
+  {
+    file: 'maj03.jpg',
+    name: 'The Empress',
+  },
+  {
+    file: 'maj04.jpg',
+    name: 'The Emperor',
+  },
+  {
+    file: 'maj05.jpg',
+    name: 'The Hierophant',
+  },
+  {
+    file: 'maj06.jpg',
+    name: 'The Lovers',
+  },
+  {
+    file: 'maj07.jpg',
+    name: 'The Chariot',
+  },
+  {
+    file: 'maj08.jpg',
+    name: 'Strength',
+  },
+  {
+    file: 'maj09.jpg',
+    name: 'The Hermit',
+  },
+  {
+    file: 'maj10.jpg',
+    name: 'Wheel of Fortune',
+  },
+  {
+    file: 'maj11.jpg',
+    name: 'Justice',
+  },
+  {
+    file: 'maj12.jpg',
+    name: 'The Hanged Man',
+  },
+  {
+    file: 'maj13.jpg',
+    name: 'Death',
+  },
+  {
+    file: 'maj14.jpg',
+    name: 'Temperance',
+  },
+  {
+    file: 'maj15.jpg',
+    name: 'The Devil',
+  },
+  {
+    file: 'maj16.jpg',
+    name: 'The Tower',
+  },
+  {
+    file: 'maj17.jpg',
+    name: 'The Star',
+  },
+  {
+    file: 'maj18.jpg',
+    name: 'The Moon',
+  },
+  {
+    file: 'maj19.jpg',
+    name: 'The Sun',
+  },
+  {
+    file: 'maj20.jpg',
+    name: 'Judgement',
+  },
+  {
+    file: 'maj21.jpg',
+    name: 'The World',
+  },
+]
